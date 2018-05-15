debe457b058395d3a38b224ae7ab159a6e419537
sls2u <sarahstewartvsp@gmail.com>
2018-05-14 22:43:52 -0400

    survey route

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,6 @@
+const path = require("path");
+module.exports = (app)=>{
+  app.get("/survey",(req, res)=>{
+    res.sendFile(path.join(__dirname + "/../public/survey.html"));
+  });
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+// =============================================================
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+// =============================================================
+require("./app/routing/htmlRoutes.js")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(3000);
